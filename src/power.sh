56f024427155b21d6e7405debfd528ee1f204e43
dgeibi <pnginside@gmail.com>
2016-02-13 17:21:33 +0800

    update

---
+++ src/power.sh
@@ -0,0 +1,24 @@
+power() {
+case "$1" in
+    lock)
+        i3lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        i3lock && systemctl suspend
+        ;;
+    hibernate)
+        i3lock && systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: power {lock|logout|suspend|hibernate|reboot|shutdown}"
+esac
+}
