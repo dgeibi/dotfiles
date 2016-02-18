2af6582a460dfbe3585218bbd01b09d2ac83a904
dgeibi <pnginside@gmail.com>
2016-02-18 12:54:12 +0800

    bug fix

--- src/install-fonts.sh
+++ src/install-fonts.sh
@@ -4,21 +4,21 @@ infonts () {
     if [ "$1" != "" ] ; then
       case $1 in
         inziu*)
-          Font_Dir=inziu-iosevka
+          export Font_Dir=inziu-iosevka
           mkdir -p $Font_Dir
           mv $1 $Font_Dir
           cd $Font_Dir || exit
           jieya $1
           cd ..
-          sudo rm -rf /usr/share/fonts/$Font_Dir
-          sudo mv $Font_Dir /usr/share/fonts/
+          sudo -E rm -rf /usr/share/fonts/$Font_Dir
+          sudo -E mv $Font_Dir /usr/share/fonts/
           sudo fc-cache -f -v
           ;;
         iosevka*.zip)
           jieya $1
-          Font_Dir=`ls | grep -v zip | grep iosevka`
-          sudo rm -rf /usr/share/fonts/$Font_Dir
-          sudo mv $Font_Dir /usr/share/fonts/
+          export Font_Dir=`ls | grep -v zip | grep iosevka`
+          sudo -E rm -rf /usr/share/fonts/$Font_Dir
+          sudo -E mv $Font_Dir /usr/share/fonts/
           sudo fc-cache -f -v
           ;;
           *) 
