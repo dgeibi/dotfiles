8b17ec6a382c071c9e6a02e3ae8f5fc06963ec6f
dgeibi <pnginside@gmail.com>
2016-03-05 13:06:05 +0800

    sync

--- src/install-fonts.sh
+++ src/install-fonts.sh
@@ -4,21 +4,18 @@ fontin () {
     if [ "$1" != "" ] ; then
       case $1 in
         inziu*)
-          sudo rm -rf /usr/share/fonts/inziu/
-          7z x $1 -oinziu
-          sudo mv inziu /usr/share/fonts/
-          cd /usr/share/fonts/
-          sudo chown -R root inziu
-          sudo chgrp -R root inziu
-          sudo chmod -R 755 inziu
-          echo "Updating fonts cache ..."
-          sudo fc-cache -f -v
+            sudo mkdir -p /usr/share/fonts/inziu/
+            sudo rm -f /usr/share/fonts/inziu/*
+            7z x "$1" -oinziu
+            sudo install -m644 -t/usr/share/fonts/inziu/ inziu/* && rm -rf inziu
+            echo "Updating fonts cache ..."
+            sudo fc-cache -f -v
           ;;
           *)
-            echo "useage: $0 <Fonts Archive File>; for inziu"
+            echo "useage: $0 <Inziu Fonts Archive>"
           ;;
       esac
     else
-        echo "useage: $0 <Fonts Archive File>; for inziu"
+        echo "useage: $0 <Inziu Fonts Archive>"
     fi
 }
