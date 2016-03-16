2352ac49d9ec1f6ce056fab121ed6ccbc9718431
dgeibi <pnginside@gmail.com>
2016-03-17 00:19:49 +0800

    sync

--- src/install-fonts.sh
+++ src/install-fonts.sh
@@ -4,14 +4,14 @@ fontin () {
     if [ "$1" != "" ] ; then
         case $1 in
             inziu*)
-                f_dir=inziu 
+                f_dir=inziu
                 ;;
-            a-nl-iosevka-slab*)
-                f_dir=nl-iosevka-slab
+            *iosevka-term-slab*)
+                f_dir=iosevka-term-slab
                 ;;
             *)
                 echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
-                exit 1
+                return 1
                 ;;
         esac
             Fonts_Dir=/usr/share/fonts/$f_dir
@@ -21,8 +21,20 @@ fontin () {
             sudo -E install -m644 -t$Fonts_Dir $f_dir/* && rm -rf $f_dir
             echo "Updating fonts cache ..."
             sudo fc-cache -f -v
-            exit 0
+            return 0
     else
         echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
+        return 1
     fi
+}
+fontdown () {
+    if [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "https://github.com/be5invis/Iosevka/releases/tag/v${1}") == '200' ] ; then
+        cd ~/Downloads
+        wget "https://github.com/be5invis/Iosevka/releases/download/v${1}/05.iosevka-term-slab-${1}.7z"
+        return 0
+    else
+        echo "Failed to download iosevka-term-slab-${1}."
+        return 1
+    fi
+
 }
