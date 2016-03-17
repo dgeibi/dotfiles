85fc3829bfd5af41cba94c499559372c32d801d2
dgeibi <pnginside@gmail.com>
2016-03-17 13:46:02 +0800

    add inziu download

--- src/install-fonts.sh
+++ src/install-fonts.sh
@@ -28,13 +28,50 @@ fontin () {
     fi
 }
 fontdown () {
-    if [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "https://github.com/be5invis/Iosevka/releases/tag/v${1}") == '200' ] ; then
-        cd ~/Downloads
-        wget "https://github.com/be5invis/Iosevka/releases/download/v${1}/05.iosevka-term-slab-${1}.7z"
-        return 0
-    else
-        echo "Failed to download iosevka-term-slab-${1}."
-        return 1
-    fi
+    forinziu() {
+        fontname="Inziu Iosevka ${2}"
+        churl="http://7xpdnl.dl1.z0.glb.clouddn.com/inziu-iosevka-${2}.7z"
+        furl=${churl}
+    }
+    forterm() {
+        fontname="Iosevka Slab Term ${2}"
+        churl="https://github.com/be5invis/Iosevka/releases/tag/v${2}"
+        furl="https://github.com/be5invis/Iosevka/releases/download/v${2}/05.iosevka-term-slab-${2}.7z"
+    }
 
+    downfont () {
+        stat=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "${churl}")
+        if [  "${stat}" == '200' ] ; then
+            echo "find ${fontname}"
+            cd ~/Downloads || exit
+            [[ $(wget "${furl}") == '0' ]] && echo "${fontname} downloaded" || echo "failed to download ${fontname}"
+        elif [ "${stat}" == '404' ] ; then
+                echo "${fontname} not found"
+            else
+                echo "Network Error."
+        fi
+    }
+    if [ "$1" != "" ] && [ "$2" != "" ] ; then
+        case $1 in
+            inziu)
+                forinziu "$@"
+                downfont
+                ;;
+            term)
+                forterm "$@"
+                downfont
+                ;;
+            both)
+                forinziu "$@"
+                downfont
+                forterm "$@"
+                downfont
+                ;;
+            *)
+                echo "Usage: $0 {inziu|term|both} <version>"
+                ;;
+        esac
+    else
+        echo "Usage: $0 {inziu|term|both} <version>"
+    fi
 }
