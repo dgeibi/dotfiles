de4c252165442a28d0fc6a3939e2e69210709755
dgeibi <png.inside@gmail.com>
2016-04-16 00:31:59 +0800

    improve fontin

--- src/install-fonts.sh
+++ src/install-fonts.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-fontin () {
-    if [ "$1" != "" ] ; then
-        case $1 in
-            inziu*)
-                f_dir=inziu
-                ;;
-            *iosevka-term-slab*)
-                f_dir=iosevka-term-slab
-                ;;
-            *)
-                echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
-                return 1
-                ;;
-        esac
-            Fonts_Dir=/usr/share/fonts/$f_dir
-            sudo -E mkdir -p $Fonts_Dir
-            sudo -E rm -f $Fonts_Dir/*
-            7z x "$1" -o$f_dir
-            sudo -E install -m644 -t$Fonts_Dir $f_dir/* && rm -rf $f_dir
-            echo "Updating fonts cache ..."
-            sudo fc-cache -f -v
-            return 0
-    else
-        echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
-        return 1
-    fi
+fontin() {
+    infont () {
+        if [ "$1" != "" ] ; then
+            case $1 in
+                inziu*)
+                    f_dir=inziu
+                    ;;
+                *iosevka-term-slab*)
+                    f_dir=iosevka-term-slab
+                    ;;
+                *)
+                    echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
+                    return 1
+                    ;;
+            esac
+                Fonts_Dir=/usr/share/fonts/$f_dir
+                sudo -E mkdir -p $Fonts_Dir
+                sudo -E rm -f $Fonts_Dir/*
+                7z x "$1" -o$f_dir
+                sudo -E install -m644 -t$Fonts_Dir $f_dir/* && rm -rf $f_dir
+                return 0
+        else
+            echo "useage: $0 <Inziu/Iosevka Fonts Archive>"
+            return 1
+        fi
+    }
+    for each in "$@"; do
+        infont "$each"
+    done
+    echo "Updating fonts cache ..."
+    sudo fc-cache -f -v
 }
 fontdown () {
     RemV=$(git ls-remote --tags https://github.com/be5invis/Iosevka.git |  awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | sed '2,$d' | sed "s/^v//")
