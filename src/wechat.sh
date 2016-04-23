1754cb7fe7a09d5f6adfa85c5cd37e0d597c14e7
dgeibi <png.inside@gmail.com>
2016-04-23 10:16:28 +0800

    uwechat: use tmp dir

--- src/wechat.sh
+++ src/wechat.sh
@@ -2,18 +2,22 @@
 # upgrade electronic-wechat
 
 uwechat(){
-    Verfile="./wechatver"
+    buildroot="$(mktemp -d)"
+    Verfile="$HOME/.wechatver"
     AURlink="https://aur.archlinux.org/electronic-wechat-git.git"
     remotever=$(git ls-remote -h -q $AURlink | awk '{print $1}')
-    if [ ! -e $Verfile ]; then
-        echo "4d82cf19340846a8140a9a19d5ee8063ab811517" > $Verfile
+    if [ ! -e "$Verfile" ]; then
+        echo "4d82cf19340846a8140a9a19d5ee8063ab811517" > "$Verfile"
     fi
-    localver=$(cat ~/.wechatver)
+    localver=$(cat "$Verfile")
     if [ "$remotever" != "$localver" ]; then
+        cd "$buildroot" || exit 1
         git clone $AURlink
-        cd electronic-wechat-git || exit
+        cd electronic-wechat-git || exit 1
         sed -i 's#cd "${_pkgname}"#\0 \&\& sed -i \"/html, body/a\\      zoom: 110%;\" \./src/inject/css\.js#' PKGBUILD
-        makepkg -sri && cd .. && rm -rf electronic-wechat-git && echo "$remotever" > "$Verfile"
+        makepkg --syncdeps --install --noconfirm && echo "$remotever" > "$Verfile"
+        cd "$HOME" || exit 1
+        rm -rf "$buildroot"
     else
         echo "electronic-wechat 已经是最新版"
     fi
