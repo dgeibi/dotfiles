11bff96f55f0dbabc8c90b681ab3e788d7aa3212
dgeibi <png.inside@gmail.com>
2016-11-30 00:36:07 +0800

    add src/op-complete.sh

---
+++ src/op-complete.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+_op()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+    case "${cur}" in
+        c*) use="conf cleanbin";;
+        co*) use="conf";;
+        f*) use="format files";;
+        fo*) use="format";;
+        fi*) use="files";;
+        b*) use="bfiles bl";;
+        bf*) use="bfiles";;
+        s*) use="sysup ss softup";;
+        sy*) use="sysup";;
+        so*) use="softup";;
+        d*) use="dns";;
+        r*) use="reboot";;
+        u*) use="upup";;
+        *) use="conf cleanbin format files bfiles sysup ss softup dns reboot upup";;
+    esac
+    COMPREPLY=( $( compgen -W "$use" -- $cur ) )
+}
+complete -o default -o nospace -F _op op
